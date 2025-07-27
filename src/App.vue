5fceb27373617ace895e682908507bda6e68fd68
abbey eleazer <abbeyeleazer@gmail.com>
2025-07-27 12:19:04 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+    </header>
+  <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
